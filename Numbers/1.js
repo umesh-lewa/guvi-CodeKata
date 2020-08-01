6ab333f2c4bda44734bd441a1777971801276787
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2020-08-01 09:09:28 +0530

    initial commit for Numbers

---
+++ Numbers/1.js
@@ -0,0 +1,62 @@
+/*
+
+you are given with array of numbers.you have to find whether array is beautiful or not. A beautiful array is an array whose sum of all numbers is divisible by 2, 3 and 5
+
+Input Description:
+You are given a number ‘n’ denoting the size of array.Next line contains n space separated numbers.
+
+Output Description:
+Print 1 if array is beautiful and 0 if it is not
+
+Sample Input :
+5
+5 25 35 -5 30
+
+Sample Output :
+1
+
+*/
+
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+
+//start-here
+
+  let num = userInput[1].split(" ");
+
+ 
+  let s = sum(num);
+  
+  if(+s % 2 == 0 && +s % 3 == 0 && +s % 5 == 0){
+     console.log("1");
+  }else{
+       console.log("0");
+       }
+     
+         
+  
+//end-here
+});
+
+function sum(input){
+             
+ if (toString.call(input) !== "[object Array]")
+    return false;
+      
+            var total =  0;
+            for(var i=0;i<input.length;i++)
+              {                  
+                if(isNaN(input[i])){
+                continue;
+                 }
+                  total += Number(input[i]);
+               }
+             return total;
+            }
