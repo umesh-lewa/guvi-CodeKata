629b7dc04818b4ff7b3a2aca6917f177eb360258
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2020-08-11 17:09:10 +0530

    find a power b

---
+++ Basics/23.js
@@ -0,0 +1,30 @@
+/*
+
+Given numbers A,B find A^B.
+Input Size : 1 <= A <= 5 <= B <= 50
+Sample Testcase :
+INPUT
+3 4
+OUTPUT
+81
+
+*/
+
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+
+//start-here
+
+  let a = userInput[0].split(" ");
+  console.log(Math.pow(+a[0], +a[1]));
+
+//end-here
+});
+
