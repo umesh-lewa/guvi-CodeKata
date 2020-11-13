4bdee1b9aef34a6e13d8eed65fc80651cafbed1c
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2020-11-13 16:52:57 +0530

    sort words

---
+++ Array/44.js
@@ -0,0 +1,50 @@
+/*
+
+Ria is a 5 year old girl. Her mother wants to teach her how to sort words in the same order that they appear in a dictionary. She decides to write a program to sort a given set of strings based on their alphabetical order. Help Ria’s mother to complete the program.
+
+
+Input Description:
+A set of N strings
+
+Output Description:
+Alphabetically sorted set of strings
+
+Sample Input :
+3
+InfinityWar EndGame Avengers
+
+Sample Output :
+Avengers EndGame InfinityWar
+
+*/
+
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+
+//start-here
+
+  let a = userInput[1].split(" ");
+  
+  a.sort();
+  
+
+      var out = "";
+      for(let j = 0 ; j < a.length ; j++){
+          if(j ==0){
+              out += a[j];
+          }else{
+            out += " " + a[j];
+          }
+      }
+
+  console.log(out);
+
+//end-here
+});
