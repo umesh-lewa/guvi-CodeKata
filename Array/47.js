250c128b2beb0d4bda33b63155c5257797fb075e
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2020-11-16 23:42:35 +0530

    minimum element array

---
+++ Array/47.js
@@ -0,0 +1,39 @@
+/*
+
+
+Prateek finds it difficult to judge the minimum element in the list of elements given to him. Your task is to develop the algorithm in order to find the minimum element.
+
+Input Description:
+You are given ‘n’ number of elements. Next line contains n space separated numbers.
+
+Output Description:
+Print the minimum element
+
+Sample Input :
+5
+3 4 9 1 6
+
+Sample Output :
+1
+
+*/
+
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+
+//start-here
+
+  let a = userInput[1].split(" ");
+  
+
+  console.log(Math.min(...a));
+
+//end-here
+});
