6bc6a2ce622813a6dace8f753b4cae9589d683a3
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2020-12-11 22:28:25 +0530

    check number between range

---
+++ Mathematics/24.js
@@ -0,0 +1,46 @@
+/*
+
+Given 3 numbers N , L and R. Print 'yes' if N is between L and R else print 'no'.
+Sample Testcase :
+INPUT
+3
+2 6
+OUTPUT
+yes
+
+*/
+
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+
+//start-here
+
+  let N = userInput[0];
+  let values = userInput[1].split(" ");
+  let L = values[0];
+  let R = values[1];
+  
+  var isPresent = false;
+
+  
+	for (var i = +L + 1; i < +R; i++) {
+      if(+i == +N){
+        isPresent=true;
+         }
+	}
+  if(isPresent){
+    console.log("yes");
+     }else{
+       console.log("no");
+  }
+
+
+//end-here
+});
