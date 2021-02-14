4ebdef55e82972c092b59e679f33684af74cc095
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-02-14 17:28:26 +0530

    Celcius convert into Fahrenheit

---
+++ Absolute Beginner/11.js
@@ -0,0 +1,38 @@
+/*
+
+You are given with a number A i.e. the temperature in Celcius. Write a program to convert this into Fahrenheit. 
+
+Note: In case of decimal values, round-off to two decimal places.
+
+Input Description:
+A number is provided in Celcius as the input of the program.
+
+Output Description:
+The output shall be the temperature converted into Fahrenheit corresponding to the input value print up to two decimal places and round off if required.
+
+Sample Input :
+12
+
+Sample Output :
+53.60
+
+*/
+
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+
+//start-here
+
+  let a = userInput[0];
+
+  console.log(+a * 9 / 5 + 32);
+
+//end-here
+});
